5badb7aa648470ee58134a9b588ddff930ad0e65
Fernandohka <fernando2005andrade@gmail.com>
2024-09-16 15:11:41 -0300

    1- menu de interação com scrool para sections da pagina

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,27 @@ function App() {
 
   return (
     <>
-    
+      <header>
+        <h1>Portfólio</h1>
+        <div>
+          <a href="#Apresentacao">Apresentação</a>
+          <a href="#Historico">Histórico</a>
+          <a href="#Projetos">Projetos</a>
+        </div>
+      </header>
+      <br />
+
+      <section id='Apresentacao'>
+        <h2>Apresentação</h2>
+      </section>
+
+      <section id='Historico'>
+        <h2>Histórico</h2>
+      </section>
+
+      <section id='Projetos'>
+        <h2>Projetos</h2>
+      </section>
     </>
   )
 }
