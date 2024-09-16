dabd8763f0b4e6061f12c6b456117b75cc48cb5d
Fernandohka <fernando2005andrade@gmail.com>
2024-09-16 16:37:50 -0300

    2- sessao com apresentação resumida profissional

--- src/App.jsx
+++ src/App.jsx
@@ -14,17 +14,23 @@ function App() {
       </header>
       <br />
 
-      <section id='Apresentacao'>
-        <h2>Apresentação</h2>
-      </section>
+      <main>
+        <section id='Apresentacao'>
+          <h2>Apresentação</h2>
+          <div>
+            <img src="../src/assets/eu.png" alt="Foto minha" />
+            <p>Meu nome é Fernando Henrique Krizanoski de Andrade, tenho 19 anos Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ea accusantium distinctio, eligendi dolorum necessitatibus. Alias quisquam vitae, incidunt rerum quos, molestias id expedita impedit fuga odit ad fugit deleniti.</p>
+          </div>
+        </section>
 
-      <section id='Historico'>
-        <h2>Histórico</h2>
-      </section>
+        <section id='Historico'>
+          <h2>Histórico</h2>
+        </section>
 
-      <section id='Projetos'>
-        <h2>Projetos</h2>
-      </section>
+        <section id='Projetos'>
+          <h2>Projetos</h2>
+        </section>
+      </main>
     </>
   )
 }
